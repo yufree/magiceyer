d1d2c648a57d5d6523bb7f284396e94b0a50ce0d
yufree <yufree@live.cn>
2020-04-15 17:11:03 -0400

    add doc

--- R/io.r
+++ R/io.r
@@ -15,15 +15,16 @@ getdepthmap <- function(path, cutoff = 0.5){
 
 #' read in image and change them into magic eye
 #' @param img cimg object
+#' @param len length of pattern square,default 50.
 #' @param shift shift of magic eye, default 0.15
 #' @return NULL
 #' @export
 
-getme <- function(img,shift=0.15){
+getme <- function(img,len = 50, shift=0.15){
         d <- dim(img)
         mask <- matrix(as.numeric(img),d[1],d[2])
         i <- imager::as.cimg(mask,dim=c(d[1],d[2],d[3],1))
-        c1 <- matrix(stats::runif(50*50,0,1),50,50)
+        c1 <- matrix(stats::runif(len*len,0,1),len,len)
         o <- getmagiceye(t(mask), c1, shift)
         o <- t(o)
         o <- imager::as.cimg(o,dim=c(d[1],d[2],d[3],1))
@@ -32,11 +33,12 @@ getme <- function(img,shift=0.15){
 
 #' read in text
 #' @param text text
+#' @param cex default 20
 #' @return NULL
 #' @export
 
-getmet <- function(text = 'yufree'){
+getmet <- function(text = 'yufree',cex = 20){
         bg <- imager::imfill(1000,1000,val=1)
-        o <- imager::implot(bg,text(500,500,text,cex=20))
+        o <- imager::implot(bg,text(500,500,text,cex=cex))
         return(1-o)
 }
